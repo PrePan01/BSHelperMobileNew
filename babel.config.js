832833db0a9be20f16c54a285aab6d6ee9f4b4cf
PrePan <1367544416@qq.com>
2022-11-04 20:32:58 +0800

    5.4.1

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
-  ]
+    // '@vue/cli-plugin-babel/preset'
+    '@vue/app',
+    [
+      '@babel/preset-env',
+      {
+        useBuiltIns: 'entry'
+      }
+    ]
+  ],
+  sourceType: 'unambiguous',
 }
